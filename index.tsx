d6633dd80c7ce4c55be5777be74f1ceae37c7b0a
joaopaulopmaues <joaopaulopmaues@gmail.com>
2022-03-07 16:46:13 -0300

    2 files modified

--- index.tsx
+++ index.tsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Tooltip from 'react-bootstrap/Tooltip';
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
@@ -23,8 +27,19 @@ class App extends Component<AppProps, AppState> {
         <p>
           Start editing to see some magic happen :)
         </p>
+        <h4>React-Bootstrap Tooltip Component</h4>
+        <OverlayTrigger
+          delay={{ hide: 450, show: 300 }}
+          overlay={(props) => (
+            <Tooltip {...props}>
+              Hii, I am a simple tooltip information!!!
+            </Tooltip>
+          )}
+          placement="bottom"
+          ><Button variant="warning">Tooltip Button</Button>
+          </OverlayTrigger>,
       </div>
-    );
+    )
   }
 }
 
